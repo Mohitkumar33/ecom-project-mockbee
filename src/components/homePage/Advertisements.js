93732a11ddb3ecc12f7260eb959642b7e3119554
Mohit kumar <46507817+Mohitkumar33@users.noreply.github.com>
2022-03-21 17:23:19 +0530

    Update Advertisements.js

--- src/components/homePage/Advertisements.js
+++ src/components/homePage/Advertisements.js
@@ -6,7 +6,7 @@ const Advertisements = () => {
   return (
     <>
       <Link to="/products">
-        <div class="advertisement-box">
+        <div className="advertisement-box">
           <img
             src="https://res.cloudinary.com/dbfzfqfhl/image/upload/v1647523467/ecom%20item%20images/home%20page%20images/crazydealss_ijqotk.webp"
             alt="advertisement_image"

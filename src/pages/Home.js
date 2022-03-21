6fba008cd5a8afbb9a3d831f011d8650c512a393
Mohit kumar <46507817+Mohitkumar33@users.noreply.github.com>
2022-03-21 16:29:41 +0530

    fix: import link form react router dom

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,18 @@
-import "./home.css"
+import "./home.css";
+import { Link } from "react-router-dom";
 
-const Home = ()=>{
-    return(
-        <>
-        <p>This is Home</p>
-        <Link to="/">Home</Link>||
+const Home = () => {
+  return (
+    <>
+      <p>This is Home</p>
+      <Link to="/">Home</Link>||
       <Link to="/products">products</Link>||
       <Link to="/wishlist">wishlist</Link>||
       <Link to="/cart">Cart</Link>||
       <Link to="/login">Login</Link>||
       <Link to="/signup">Signup</Link>
-        </>
-    )
-}
+    </>
+  );
+};
 
-export {Home}
+export { Home };
